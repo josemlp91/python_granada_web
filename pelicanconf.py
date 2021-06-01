4ab94803bc5995cad6c76e388499ff044522d52b
unknown <pablojpba@gmail.com>
2021-06-01 10:57:43 -0300

    articulo + plugins para jupyter + plugin para bokehjs en base.html

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
+from pelican_jupyter import markup as nb_markup
+
 AUTHOR = 'Python Granada Org'
 SITENAME = 'Python Granada'
 SITEURL = ''
@@ -19,7 +21,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 PLUGIN_PATHS = ["plugins"]
-PLUGINS = ["i18n_subsites", "assets"]
+PLUGINS = ["i18n_subsites", "assets", nb_markup]
 
 JINJA_ENVIRONMENT = {
     "extensions": ["jinja2.ext.i18n"],
@@ -44,3 +46,15 @@ MENUITEMS_NAVBAR = (("Asociación", f"{SITEURL}/pages/about.html"),)
 NAVBAR_STYLE = "is-primary"
 THEME_LOGO = f"{SITEURL}/theme/images/logo_grande.svg"
 FOOTER= "Made with ❤️ with Python from Granada. Under construction 🚧"
+
+# Pelican jupyter integration
+
+IGNORE_FILES = [".ipynb_checkpoints"]
+# IPYNB_SKIP_CSS=True
+IPYNB_FIX_CSS = True
+IPYNB_SKIP_CSS = False
+
+IPYNB_STOP_SUMMARY_TAGS = [('div', ('class', 'input')), ('div', ('class', 'output')), ('h2', ('id', 'Header-2'))]
+IPYNB_GENERATE_SUMMARY = True
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# IPYNB_EXPORT_TEMPLATE =  "minimal_xy_theme/templates/basic_jupyter.html"
